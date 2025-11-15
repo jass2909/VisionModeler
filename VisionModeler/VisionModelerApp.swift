8701d30bb337eb1ac2ff867604a30351e5119f6d
jasmeetsingh <jasmeet@gmx.at>
2025-11-15 20:58:59 +0100

    Initial Commit

---
+++ VisionModeler/VisionModelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionModelerApp.swift
+//  VisionModeler
+//
+//  Created by jasmeet singh on 15.11.25.
+//
+
+import SwiftUI
+
+@main
+struct VisionModelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionModelerTests/VisionModelerTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionModelerTests.swift
+//  VisionModelerTests
+//
+//  Created by jasmeet singh on 15.11.25.
+//
+
+import Testing
+@testable import VisionModeler
+
+struct VisionModelerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
